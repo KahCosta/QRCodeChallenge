7b30322c24ef605080f5a8c19f5922af86ab3bfe
Kamilla Costa Souza <kamillacostasouza@hotmail.com>
2020-09-13 01:08:50 -0300

    Adc cors

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,16 +1,17 @@
 import { hash } from 'bcryptjs';
-import { request, response, Router } from 'express';
+import { Router } from 'express';
 
 const transactionRouter = Router();
 
-
 transactionRouter.post('/', async (request, response) => {
-
+  // eslint-disable-next-line prettier/prettier
   const {body} = request;
 
+  // eslint-disable-next-line prettier/prettier
   try {
 
     const qrCodeString = await hash(JSON.stringify(body), 10);
+    // eslint-disable-next-line prettier/prettier
     return response.json({ qrCodeString });
 
   } catch (err) {
